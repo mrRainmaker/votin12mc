727c6e2c0b033a42ffa9aff56ada1a3a9bd0bab0
mrRainmaker <xytomg@gmail.com>
2016-05-20 12:13:33 +0100

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -9,7 +9,7 @@ pg.connect(connectionString, function(err, client, done) {
    });
 });;
 
-var client = new pg.Client(c"postgres://*kjakjjfpjggjja*:*3Al9QFqYeuzeSUK1cCWE3osXsi@ec2-54-228-226-93.eu-west-1.compute.amazonaws.com*:*5432*:/*d9ger7jvq3mgd9*");
+var client = new pg.Client("postgres://*kjakjjfpjggjja*:*3Al9QFqYeuzeSUK1cCWE3osXsi@ec2-54-228-226-93.eu-west-1.compute.amazonaws.com*:*5432*:/*d9ger7jvq3mgd9*");
 client.connect();
 var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 query.on('end', function() { client.end(); });
