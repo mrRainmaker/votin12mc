9e9ade9742d5b5f35cdae6ff1612960b38102eb6
mrRainmaker <xytomg@gmail.com>
2016-05-20 12:08:43 +0100

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,11 +1,13 @@
 var pg = require('pg');
-//var connectionString = require(path.join(__dirname, '../', '../', 'config'));
+var connectionString = "postgres://kjakjjfpjggjja:3Al9QFqYeuzeSUK1cCWE3osXsi@ec2-54-228-226-93.eu-west-1.compute.amazonaws.com:5432:/d9ger7jvq3mgd9"
 
-pg.defaults.ssl = true;
-pg.connect(process.env.postgres://kjakjjfpjggjja:3Al9QFqYeuzeSUK1cCWE3osXsi@ec2-54-228-226-93.eu-west-1.compute.amazonaws.com:5432/d9ger7jvq3mgd9
-, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+pg.connect(connectionString, function(err, client, done) {
+   client.query('SELECT * FROM your_table', function(err, result) {
+      done();
+      if(err) return console.error(err);
+      console.log(result.rows);
+   });
+});;
 
 var client = new pg.Client(connectionString);
 client.connect();
