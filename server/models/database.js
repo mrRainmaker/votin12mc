ee721f2ea22b060f645485129ead34291706abb8
mrRainmaker <xytomg@gmail.com>
2016-05-20 14:36:55 +0100

    Update database.js

--- server/models/database.js
+++ server/models/database.js
@@ -1,21 +1,7 @@
 var pg = require('pg');
-var conString = "postgres://kjakjjfpjggjja:3Al9QFqYeuzeSUK1cCWE3osXsi@localhost/d9ger7jvq3mgd9";
- 
-//this initializes a connection pool 
-//it will keep idle connections open for a (configurable) 30 seconds 
-//and set a limit of 10 (also configurable) 
-pg.connect(conString, function(err, client, done) {
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-    //call `done()` to release the client back to the pool 
-    done();
- 
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].number);
-    //output: 1 
-  });
-});
+var connectionString = "postgres://kjakjjfpjggjja:3Al9QFqYeuzeSUK1cCWE3osXsi@localhost/dev"
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() { client.end(); });
