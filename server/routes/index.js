5b3404bca2a692b102c8b972b89113567d1e6e97
mrRainmaker <xytomg@gmail.com>
2016-05-23 10:16:15 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,10 +29,10 @@ router.post('/api/v1/todos', function(req, res) {
         }
 
         // SQL Query > Insert Data
-        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
+        client.query("INSERT INTO todo(id, vote) values($1, $2)", [data.text, data.complete]);
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+        var query = client.query("SELECT * FROM todo ORDER BY id ASC");
 
         // Stream results back one row at a time
         query.on('row', function(row) {
